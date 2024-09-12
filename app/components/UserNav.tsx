54a658107cd56a467842de2a1b666f4a29bdf53d
Subhampreet <subhampreet18@gmail.com>
2024-09-13 01:00:30 +0530

    Build 0.0.2 : Navbar UI

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,23 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+
+export default function UserNav() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+                    <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+                    <img
+                        src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+                        alt="Image of the user"
+                        className="rounded-full h-8 w-8 hidden lg:block"
+                    />
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuItem>Login</DropdownMenuItem>
+                <DropdownMenuItem>Register</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
