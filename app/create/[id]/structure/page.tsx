5beff1daa446c264ce196ab1e21a1200413e3841
Subhampreet <subhampreet18@gmail.com>
2024-09-20 23:35:08 +0530

    Build 0.0.7 : SelectedCategory Component Added

--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,4 +1,5 @@
 import SelectedCategory from '@/app/components/SelectedCategory'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function StructureRoute() {
@@ -12,6 +13,12 @@ export default function StructureRoute() {
 
         <form>
             <SelectedCategory />
+
+            <div className='fixed w-full bottom-0 z-10 bg-white border-t h-24'>
+              <div className='flex items-center justify-between mx-auto px-5 lg:px-10 h-full'>
+                <Button>Cancel</Button>
+              </div>
+            </div>
         </form>
     </>
   )
