b353da4720417cb328e7719ad4a466fab3811d61
Subhampreet <subhampreet18@gmail.com>
2024-09-22 13:25:00 +0530

    Build 0.1.4 : Create Address Initialized

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,7 +1,31 @@
+import { Select, SelectContent, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>Addressssssssssss</div>
+    <>
+        <div className='w-3/5 mx-auto'>
+            <h2 className='text-3xl font-semibold tracking-tight transition-colors mb-10'>
+                Where is your Home located?
+            </h2>
+        </div>
+
+        <form>
+            <div className='w-3/5 mx-auto'>
+                <div className='mb-5'>
+                    <Select required>
+                        <SelectTrigger className='w-full'>
+                            <SelectValue placeholder="Select a Country" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Countries</SelectLabel>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+        </form>
+    </>
   )
 }
