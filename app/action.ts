ec0409226e5ff3f6d739401aae3a4b04e59fd884
Subhampreet <subhampreet18@gmail.com>
2024-09-27 23:31:22 +0530

    Build 0.1.7 : Location Server Action

--- app/action.ts
+++ app/action.ts
@@ -85,3 +85,19 @@ export async function CreateDescription(formData: FormData){
 
     return redirect(`/create/${homeId}/address`);
 }
+
+
+export async function createLocation(formData: FormData) {
+    const homeId = formData.get("homeId") as string
+    const countryValue = formData.get("countryValue") as string
+
+    const data = await prisma.home.update({
+        where : {
+            id: homeId,
+        },
+        data : {
+            addedLocation: true,
+            country: countryValue,
+        },
+    });
+}
