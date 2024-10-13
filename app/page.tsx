4f1f4552e11dc208dd68e317fc343409dea69a07
Subhampreet <subhampreet18@gmail.com>
2024-10-13 11:41:26 +0530

    Build 0.2.4 : No Filtered Items View Added

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@ import ListingCard from "./components/ListingCard";
 import MapFilterItem from "./components/MapFilterItem";
 import prisma from "./lib/db";
 import { SkeletonCard } from "./components/SkeletonCard";
+import { NoItems } from "./components/NoItem";
 
 
-async function getData({searchParams, } : {searchParams?:{filter?:string}; }) {
+async function getData({ searchParams, }: { searchParams?: { filter?: string }; }) {
   const data = await prisma.home.findMany({
     where: {
       addedCategory: true,
       addedDescription: true,
       addedLocation: true,
       categoryName: searchParams?.filter ?? undefined
-    }, select : {
+    }, select: {
       photo: true,
       id: true,
       price: true,
@@ -26,35 +27,44 @@ async function getData({searchParams, } : {searchParams?:{filter?:string}; }) {
 
 
 
-export default function Home({searchParams, } : {searchParams?:{filter?:string}; }) {
+export default function Home({ searchParams, }: { searchParams?: { filter?: string }; }) {
 
   return (
     <div className="container mx-auto px-5 lg:px-10 mb-10">
       <MapFilterItem />
 
       <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
-        <ShowItems searchParams={searchParams} />      
+        <ShowItems searchParams={searchParams} />
       </Suspense>
     </div>
   );
 }
 
 
-async function ShowItems({searchParams, } : {searchParams?:{filter?:string}; }) {
-  const data = await getData({searchParams: searchParams});
+async function ShowItems({ searchParams, }: { searchParams?: { filter?: string }; }) {
+  const data = await getData({ searchParams: searchParams });
 
   return (
-    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
-        {
-          data.map((item) => (
-            <ListingCard key={item.id} description={item.description as string} imagePath={item.photo as string} location={item.country as string} price={item.price as number} />
-          ))
-        }
-    </div>
+    <>
+      {
+        data.length === 0 ? (
+          <NoItems description="Please check a other category or create your own listing!"
+          title="Sorry no listings found for this category..." />
+        ) : (
+          <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+            {
+              data.map((item) => (
+                <ListingCard key={item.id} description={item.description as string} imagePath={item.photo as string} location={item.country as string} price={item.price as number} />
+              ))
+            }
+          </div>
+        )
+      }
+    </>
   )
 }
 
-function SkeletonLoading () {
+function SkeletonLoading() {
   return (
     <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
       <SkeletonCard />
